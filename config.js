4551e64837d42fb377357dfdb578546a25c86ce3
AmbastSagarika <75776367+AmbastSagarika@users.noreply.github.com>
2021-10-14 18:57:44 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBVGVokldNjrutqYfz7DbyjnB4dhvJpBMA",
+  authDomain: "storytellerapp-45705.firebaseapp.com",
+  databaseURL: "https://storytellerapp-45705-default-rtdb.firebaseio.com",
+  projectId: "storytellerapp-45705",
+  storageBucket: "storytellerapp-45705.appspot.com",
+  messagingSenderId: "964017907278",
+  appId: "1:964017907278:web:d0bae54766ad155aeb851f"
+};
